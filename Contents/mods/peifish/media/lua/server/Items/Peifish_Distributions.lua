8bd5bc24a692c27d5c5c5bbecd6e2c4a5d6aa608
Andy <aeudet@gmail.com>
2024-01-20 15:32:50 -0500

    Partially working pei fishing

---
+++ Contents/mods/peifish/media/lua/server/Items/Peifish_Distributions.lua
@@ -0,0 +1,141 @@
+require 'Items/ProceduralDistributions'
+                                                                
+table.insert(ProceduralDistributions["list"]["ButcherFish"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["ButcherFish"].items, 5);
+table.insert(ProceduralDistributions["list"]["ButcherFish"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["ButcherFish"].items, 5);
+table.insert(ProceduralDistributions["list"]["ButcherFish"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["ButcherFish"].items, 5);
+
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items, 6);
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["ButcherFreezer"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items, 2);
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items, 2);
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["ChineseKitchenFreezer"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["DeepFryKitchenFreezer"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["DinerKitchenFreezer"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenButcher"].items, 1);
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenButcher"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenButcher"].items, 1);
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenButcher"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenButcher"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenFreezer"].items, 3);
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenFreezer"].items, 2);
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["FishChipsKitchenFreezer"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, 4);
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, 3);
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["FreezerGeneric"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items, 6);
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items, 4);
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["FreezerRich"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items, 2);
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items, 1);
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["FreezerTrailerPark"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["ItalianKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenButcher"].items, 3);
+table.insert(ProceduralDistributions["list"]["ItalianKitchenButcher"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenButcher"].items, 1);
+table.insert(ProceduralDistributions["list"]["ItalianKitchenButcher"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenButcher"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items, 4);
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items, 2);
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["ItalianKitchenFreezer"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["JaysKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["JaysKitchenButcher"].items, 1);
+table.insert(ProceduralDistributions["list"]["JaysKitchenButcher"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["JaysKitchenButcher"].items, 1);
+table.insert(ProceduralDistributions["list"]["JaysKitchenButcher"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["JaysKitchenButcher"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items, 1);
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["JaysKitchenFreezer"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MexicanKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["MexicanKitchenButcher"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MexicanKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["MexicanKitchenFreezer"].items, 3);
+
+table.insert(ProceduralDistributions["list"]["RestaurantKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["RestaurantKitchenFreezer"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenButcher"].items, 9);
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenButcher"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenButcher"].items, 9);
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenButcher"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenButcher"].items, 9);
+
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items, 9);
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items, 9);
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["SeafoodKitchenFreezer"].items, 9);
+
+table.insert(ProceduralDistributions["list"]["StoreKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["StoreKitchenButcher"].items, 2);
+table.insert(ProceduralDistributions["list"]["StoreKitchenButcher"].items,"Base.EMFSMilkFish");
+table.insert(ProceduralDistributions["list"]["StoreKitchenButcher"].items, 2);
+table.insert(ProceduralDistributions["list"]["StoreKitchenButcher"].items,"Base.EMFSRedSnapper");
+table.insert(ProceduralDistributions["list"]["StoreKitchenButcher"].items, 2);
+
+table.insert(ProceduralDistributions["list"]["SushiKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["SushiKitchenFreezer"].items, 5);
+
+table.insert(ProceduralDistributions["list"]["TheatreKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["TheatreKitchenFreezer"].items, 3);
+
+table.insert(ProceduralDistributions["list"]["WesternKitchenButcher"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["WesternKitchenButcher"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["WesternKitchenFreezer"].items,"Base.EMFSTilapia");
+table.insert(ProceduralDistributions["list"]["WesternKitchenFreezer"].items, 1);
