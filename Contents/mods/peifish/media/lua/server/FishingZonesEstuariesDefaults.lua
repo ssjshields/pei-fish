0a1a9039485919f3addecb23782d3ba22350eed0
ssjshields <ssjshields@outlook.com>
2024-01-22 00:41:49 -0500

    Create FishingZonesEstuariesDefaults.lua

---
+++ Contents/mods/peifish/media/lua/server/FishingZonesEstuariesDefaults.lua
@@ -0,0 +1,28 @@
+local FishingZonesEstuariesDefaults = {}
+local FishingZonesEstuaries = ModData.getOrCreate("FishingZonesEstuaries_zones")
+    if #FishingZonesEstuaries > 0 then return end
+
+FishingZonesEstuariesDefaults.Zone = {
+	FishingZonesEstuariesDefaults.addZone("A1", 1329, 2238, 1377, 2259, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A2", 1377, 2221, 1400, 2257, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A3", 0, 0, 0, 0, FishingZonesEstuaries) -- left off https://discord.com/channels/@me/1198112697594679376/1198856611788230716
+	FishingZonesEstuariesDefaults.addZone("A4", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A5", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A6", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A7", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A8", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A9", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A10", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A11", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A12", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A13", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A14", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A15", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A16", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A17", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A18", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A19", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A20", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("A21", 0, 0, 0, 0, FishingZonesEstuaries)
+	FishingZonesEstuariesDefaults.addZone("B1", 0, 0, 0, 0, FishingZonesEstuaries)
+return FishingZonesEstuariesDefaults
