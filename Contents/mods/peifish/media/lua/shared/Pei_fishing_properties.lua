ce940e261d46726abe1e4f8bceb7bdbc504ec2d2
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-23 21:19:32 -0500

    Update Pei_fishing_properties.lua

--- Contents/mods/peifish/media/lua/shared/Pei_fishing_properties.lua
+++ Contents/mods/peifish/media/lua/shared/Pei_fishing_properties.lua
@@ -1,5 +1,32 @@
 require "Fishing/fishing_properties"
 
+local classifyFish = {
+    ["Lobster"] = "ground",
+}
+
+local _removedFishes = {}
+local removedFishes = {
+    "Pike",
+    "Trout",
+    "CatFish",
+    "Bass",
+    "Crappie fish",
+    "PanFish",
+    "Perch",
+    "Walleye",
+}
+
+for _,fish in pairs(removedFishes) do _removedFishes[fish] = true end
+
+for i = #fishes, 1, -1 do
+    if _removedFishes[fishes[i].name] then
+        table.remove(fishes, i)
+    elseif classifyFish[fishes[i].name] then
+        fishes[i].classification = classifyFish[fishes[i].name]
+    end
+end
+
+
 --- Atlantic Cod :
 local AtlanticCod = {}
 AtlanticCod.name = "Atlantic Cod"
@@ -8,6 +35,7 @@ AtlanticCod.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackl
 AtlanticCod.little = {minSize = 23, maxSize = 40, weightChange = 60}
 AtlanticCod.medium = {minSize = 40, maxSize = 65, weightChange = 45}
 AtlanticCod.big    = {minSize = 65, maxSize = 75, weightChange = 30}
+AtlanticCod.classification = "estuaries" --ground
 table.insert(fishes, AtlanticCod)
 
 --- Atlantic Halibut :
@@ -18,6 +46,7 @@ AtlanticHalibut.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingT
 AtlanticHalibut.little = {minSize = 33, maxSize = 48, weightChange = 60}
 AtlanticHalibut.medium = {minSize = 48, maxSize = 58, weightChange = 45}
 AtlanticHalibut.big    = {minSize = 58, maxSize = 100, weightChange = 16}
+AtlanticHalibut.classification = "estuaries" --ground
 table.insert(fishes, AtlanticHalibut)
 
 --- Atlantic Herring :
@@ -28,6 +57,7 @@ AtlanticHerring.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingT
 AtlanticHerring.little = {minSize = 10, maxSize = 19, weightChange = 30}
 AtlanticHerring.medium = {minSize = 19, maxSize = 33, weightChange = 20}
 AtlanticHerring.big    = {minSize = 33, maxSize = 40, weightChange = 10}
+AtlanticHerring.classification = "estuaries" --ground
 table.insert(fishes, AtlanticHerring)
 
 --- AtlanticMackerel :
@@ -38,6 +68,7 @@ AtlanticMackerel.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "Fishing
 AtlanticMackerel.little = {minSize = 11, maxSize = 25, weightChange = 30}
 AtlanticMackerel.medium = {minSize = 25, maxSize = 35, weightChange = 20}
 AtlanticMackerel.big    = {minSize = 35, maxSize = 45, weightChange = 10}
+AtlanticMackerel.classification = "estuaries" --ground
 table.insert(fishes, AtlanticMackerel)
 
 --- AtlanticSalmon :
@@ -48,6 +79,7 @@ AtlanticSalmon.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTa
 AtlanticSalmon.little = {minSize = 25, maxSize = 65, weightChange = 50}
 AtlanticSalmon.medium = {minSize = 65, maxSize = 75, weightChange = 40}
 AtlanticSalmon.big    = {minSize = 75, maxSize = 85, weightChange = 30}
+AtlanticSalmon.classification = "estuaries" --ground
 table.insert(fishes, AtlanticSalmon)
 
 --- AtlanticTomcod :
@@ -58,6 +90,7 @@ AtlanticTomcod.lure = {"Worm", "Baitfish", "Cricket", "Grasshopper", "Cockroach"
 AtlanticTomcod.little = {minSize = 10, maxSize = 15, weightChange = 30}
 AtlanticTomcod.medium = {minSize = 15, maxSize = 20, weightChange = 20}
 AtlanticTomcod.big    = {minSize = 20, maxSize = 30, weightChange = 10}
+AtlanticTomcod.classification = "estuaries" --ground
 table.insert(fishes, AtlanticTomcod)
 
 --- SummerFlounder :
@@ -68,6 +101,7 @@ SummerFlounder.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTa
 SummerFlounder.little = {minSize = 20, maxSize = 40, weightChange = 30}
 SummerFlounder.medium = {minSize = 40, maxSize = 50, weightChange = 20}
 SummerFlounder.big    = {minSize = 50, maxSize = 65, weightChange = 10}
+SummerFlounder.classification = "estuaries" --ground
 table.insert(fishes, SummerFlounder)
 
 --- Haddock :
@@ -78,6 +112,7 @@ Haddock.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", 
 Haddock.little = {minSize = 11, maxSize = 25, weightChange = 50}
 Haddock.medium = {minSize = 25, maxSize = 45, weightChange = 40}
 Haddock.big    = {minSize = 45, maxSize = 80, weightChange = 30}
+Haddock.classification = "estuaries" --ground
 table.insert(fishes, Haddock)
 
 --- Lumpfish :
@@ -88,6 +123,7 @@ Lumpfish.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle",
 Lumpfish.little = {minSize = 15, maxSize = 30, weightChange = 30}
 Lumpfish.medium = {minSize = 30, maxSize = 45, weightChange = 20}
 Lumpfish.big    = {minSize = 45, maxSize = 50, weightChange = 10}
+Lumpfish.classification = "estuaries" --ground
 table.insert(fishes, Lumpfish)
 
 --- Pollack :
@@ -98,6 +134,7 @@ Pollack.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", 
 Pollack.little = {minSize = 25, maxSize = 35, weightChange = 40}
 Pollack.medium = {minSize = 35, maxSize = 75, weightChange = 30}
 Pollack.big    = {minSize = 75, maxSize = 90, weightChange = 20}
+Pollack.classification = "estuaries" --ground
 table.insert(fishes, Pollack)
 
 --- RainbowSmelt :
@@ -108,6 +145,7 @@ RainbowSmelt.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTack
 RainbowSmelt.little = {minSize = 10, maxSize = 18, weightChange = 30}
 RainbowSmelt.medium = {minSize = 18, maxSize = 23, weightChange = 20}
 RainbowSmelt.big = {minSize = 23, maxSize = 30, weightChange = 10}
+RainbowSmelt.classification = "estuaries" --ground
 table.insert(fishes, RainbowSmelt)
 
 --- SpinyDogfish :
@@ -115,18 +153,10 @@ local SpinyDogfish = {}
 SpinyDogfish.name = "Spiny Dogfish"
 SpinyDogfish.item = "Base.SpinyDogfish"
 SpinyDogfish.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-SpinyDogfish.little = {}
-SpinyDogfish.medium = {}
-SpinyDogfish.big = {}
-SpinyDogfish.little.minSize = 30
-SpinyDogfish.little.maxSize = 45
-SpinyDogfish.medium.minSize = 45
-SpinyDogfish.medium.maxSize = 65
-SpinyDogfish.big.minSize = 65
-SpinyDogfish.big.maxSize = 100
-SpinyDogfish.little.weightChange = 50
-SpinyDogfish.medium.weightChange = 40
-SpinyDogfish.big.weightChange = 30
+SpinyDogfish.little = {minSize = 30, maxSize = 45, weightChange = 50}
+SpinyDogfish.medium = {minSize = 45, maxSize = 65, weightChange = 40}
+SpinyDogfish.big = {minSize = 65, maxSize = 100, weightChange = 30}
+SpinyDogfish.classification = "estuaries" --ground
 table.insert(fishes, SpinyDogfish)
 
 --- StripedBass :
@@ -134,18 +164,10 @@ local StripedBass = {}
 StripedBass.name = "Striped Bass"
 StripedBass.item = "Base.StripedBass"
 StripedBass.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-StripedBass.little = {}
-StripedBass.medium = {}
-StripedBass.big = {}
-StripedBass.little.minSize = 20
-StripedBass.little.maxSize = 35
-StripedBass.medium.minSize = 35
-StripedBass.medium.maxSize = 65
-StripedBass.big.minSize = 65
-StripedBass.big.maxSize = 120
-StripedBass.little.weightChange = 50
-StripedBass.medium.weightChange = 40
-StripedBass.big.weightChange = 30
+StripedBass.little = {minSize = 20, maxSize = 35, weightChange = 50}
+StripedBass.medium = {minSize = 35, maxSize = 65, weightChange = 40}
+StripedBass.big = {minSize = 65, maxSize = 120, weightChange = 30}
+StripedBass.classification = "estuaries" --ground
 table.insert(fishes, StripedBass)
 
 --- WhitePerch :
@@ -153,18 +175,10 @@ local WhitePerch = {}
 WhitePerch.name = "White Perch"
 WhitePerch.item = "Base.WhitePerch"
 WhitePerch.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-WhitePerch.little = {}
-WhitePerch.medium = {}
-WhitePerch.big = {}
-WhitePerch.little.minSize = 16
-WhitePerch.little.maxSize = 30
-WhitePerch.medium.minSize = 30
-WhitePerch.medium.maxSize = 40
-WhitePerch.big.minSize = 40
-WhitePerch.big.maxSize = 50
-WhitePerch.little.weightChange = 30
-WhitePerch.medium.weightChange = 20
-WhitePerch.big.weightChange = 10
+WhitePerch.little = {minSize = 16, maxSize = 30, weightChange = 30}
+WhitePerch.medium = {minSize = 30, maxSize = 40, weightChange = 20}
+WhitePerch.big = {minSize = 40, maxSize = 50, weightChange = 10}
+WhitePerch.classification = "estuaries" --ground
 table.insert(fishes, WhitePerch)
 
 --- WinterFlounder :
@@ -172,37 +186,23 @@ local WinterFlounder = {}
 WinterFlounder.name = "Winter Flounder"
 WinterFlounder.item = "Base.WinterFlounder"
 WinterFlounder.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-WinterFlounder.little = {}
-WinterFlounder.medium = {}
-WinterFlounder.big = {}
-WinterFlounder.little.minSize = 25
-WinterFlounder.little.maxSize = 50
-WinterFlounder.medium.minSize = 50
-WinterFlounder.medium.maxSize = 65
-WinterFlounder.big.minSize = 65
-WinterFlounder.big.maxSize = 75
-WinterFlounder.little.weightChange = 40
-WinterFlounder.medium.weightChange = 30
-WinterFlounder.big.weightChange = 20
+WinterFlounder.little = {minSize = 25, maxSize = 50, weightChange = 40}
+WinterFlounder.medium = {minSize = 50, maxSize = 65, weightChange = 30}
+WinterFlounder.big = {minSize = 65, maxSize = 75, weightChange = 20}
+WinterFlounder.classification = "estuaries" --ground
 table.insert(fishes, WinterFlounder)
 
+
+--GROUND FISH
 --- BrookTrout :
 local BrookTrout = {}
 BrookTrout.name = "Brook Trout"
 BrookTrout.item = "Base.BrookTrout"
 BrookTrout.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-BrookTrout.little = {}
-BrookTrout.medium = {}
-BrookTrout.big = {}
-BrookTrout.little.minSize = 12
-BrookTrout.little.maxSize = 35
-BrookTrout.medium.minSize = 35
-BrookTrout.medium.maxSize = 65
-BrookTrout.big.minSize = 65
-BrookTrout.big.maxSize = 75
-BrookTrout.little.weightChange = 50
-BrookTrout.medium.weightChange = 40
-BrookTrout.big.weightChange = 30
+BrookTrout.little = {minSize = 12, maxSize = 35, weightChange = 50}
+BrookTrout.medium = {minSize = 35, maxSize = 65, weightChange = 40}
+BrookTrout.big = {minSize = 65, maxSize = 75, weightChange = 30}
+BrookTrout.classification = "ground"
 table.insert(fishes, BrookTrout)
 
 --- BrownTrout :
@@ -210,18 +210,10 @@ local BrownTrout = {}
 BrownTrout.name = "Brown Trout"
 BrownTrout.item = "Base.BrownTrout"
 BrownTrout.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-BrownTrout.little = {}
-BrownTrout.medium = {}
-BrownTrout.big = {}
-BrownTrout.little.minSize = 25
-BrownTrout.little.maxSize = 35
-BrownTrout.medium.minSize = 35
-BrownTrout.medium.maxSize = 55
-BrownTrout.big.minSize = 55
-BrownTrout.big.maxSize = 65
-BrownTrout.little.weightChange = 40
-BrownTrout.medium.weightChange = 30
-BrownTrout.big.weightChange = 20
+BrownTrout.little = {minSize = 25, maxSize = 35, weightChange = 40}
+BrownTrout.medium = {minSize = 35, maxSize = 55, weightChange = 30}
+BrownTrout.big = {minSize = 55, maxSize = 65, weightChange = 20}
+BrownTrout.classification = "ground"
 table.insert(fishes, BrownTrout)
 
 --- Gaspereau :
@@ -229,35 +221,21 @@ local Gaspereau = {}
 Gaspereau.name = "Gaspereau"
 Gaspereau.item = "Base.Gaspereau"
 Gaspereau.lure = {"Worm", "Cricket", "Grasshopper", "Cockroach", "FishingTackle", "FishingTackle2"}
-Gaspereau.little = {}
-Gaspereau.medium = {}
-Gaspereau.big = {}
-Gaspereau.little.minSize = 10
-Gaspereau.little.maxSize = 20
-Gaspereau.medium.minSize = 20
-Gaspereau.medium.maxSize = 25
-Gaspereau.big.minSize = 25
-Gaspereau.big.maxSize = 30
-Gaspereau.little.weightChange = 30
-Gaspereau.medium.weightChange = 20
-Gaspereau.big.weightChange = 10
+Gaspereau.little = {minSize = 10, maxSize = 20, weightChange = 30}
+Gaspereau.medium = {minSize = 20, maxSize = 25, weightChange = 20}
+Gaspereau.big = {minSize = 25, maxSize = 30, weightChange = 10}
+Gaspereau.classification = "ground"
 table.insert(fishes, Gaspereau)
 
+
+--CAT FISH
 --- Brown Bullhead Catfish :
 local BrownBullheadCatfish = {}
 BrownBullheadCatfish.name = "Brown Bullhead Catfish"
 BrownBullheadCatfish.item = "Base.BrownBullheadCatfish"
 BrownBullheadCatfish.lure = {"Worm", "BaitFish"}
-BrownBullheadCatfish.little = {}
-BrownBullheadCatfish.medium = {}
-BrownBullheadCatfish.big = {}
-BrownBullheadCatfish.little.minSize = 25
-BrownBullheadCatfish.little.maxSize = 45
-BrownBullheadCatfish.medium.minSize = 45
-BrownBullheadCatfish.medium.maxSize = 55
-BrownBullheadCatfish.big.minSize = 55
-BrownBullheadCatfish.big.maxSize = 65
-BrownBullheadCatfish.little.weightChange = 30
-BrownBullheadCatfish.medium.weightChange = 20
-BrownBullheadCatfish.big.weightChange = 10
+BrownBullheadCatfish.little = {minSize = 25, maxSize = 45, weightChange = 30}
+BrownBullheadCatfish.medium = {minSize = 45, maxSize = 55, weightChange = 20}
+BrownBullheadCatfish.big = {minSize = 55, maxSize = 65, weightChange = 10}
+BrownBullheadCatfish.classification = "catfish"
 table.insert(fishes, BrownBullheadCatfish)
