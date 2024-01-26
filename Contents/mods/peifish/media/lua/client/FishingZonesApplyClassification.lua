f65f2a1352f39f23208056f8be3a31b3f3f2b034
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-26 17:54:59 -0500

    classify fish on fishzone end

--- Contents/mods/peifish/media/lua/client/FishingZonesApplyClassification.lua
+++ Contents/mods/peifish/media/lua/client/FishingZonesApplyClassification.lua
@@ -2,14 +2,37 @@ local FishingZonesApplyClassifications = {}
 
 require "client/Fishing/FishingManager"
 
-function FishingZonesApplyClassifications.apply()
-    local fishes = Fishing.fishes
-    Fishing.fishesByZone = {}
+FishingZonesApplyClassifications._zoneToFish = {}
+
+FishingZonesApplyClassifications._zoneToFish["estuaries"] = {
+    "Atlantic Cod", "Atlantic Halibut", "Atlantic Herring", "Atlantic Mackerel", "Atlantic Salmon",
+    "Atlantic Tomcod", "Summer Flounder", "Haddock", "Lumpfish", "Pollack", "Rainbow Smelt",
+    "Spiny Dogfish", "Striped Bass", "White Perch", "Winter Flounder"
+}
+
+FishingZonesApplyClassifications._zoneToFish["ground"] = {"Brook Trout", "Brown Trout", "Gaspereau"}
+
+FishingZonesApplyClassifications._zoneToFish["catfish"] = {"Brown Bullhead Catfish"}
+
+---Load temp list as true for easier compare
+FishingZonesApplyClassifications.zoneToFish = {}
+for zone,fishes in pairs(FishingZonesApplyClassifications._zoneToFish) do
+    FishingZonesApplyClassifications.zoneToFish[zone] = {}
     for _,fish in pairs(fishes) do
-        local classification = fish.classification
-        if classification then
-            Fishing.fishesByZone[classification] = Fishing.fishesByZone[classification] or {}
-            table.insert(Fishing.fishesByZone[classification], fish)
+        FishingZonesApplyClassifications.zoneToFish[zone][fish] = true
+    end
+end
+
+
+function FishingZonesApplyClassifications.apply()
+    local FishingFish = Fishing.fishes
+    Fishing.fishesByZone = {}
+    for _,fish in pairs(FishingFish) do
+        for zone,fishes in pairs(FishingZonesApplyClassifications.zoneToFish) do
+            if fishes[fish.name] then
+                Fishing.fishesByZone[zone] = Fishing.fishesByZone[zone] or {}
+                table.insert(Fishing.fishesByZone[zone], fish)
+            end
         end
     end
 end
