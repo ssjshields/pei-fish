d925266fc2cd45f17569793103cce657e2b65e6f
Chuckleberry Finn <6071161+Chuckleberry-Finn@users.noreply.github.com>
2024-01-25 21:38:36 -0500

    Update FishingZonesApplyClassification.lua

--- Contents/mods/peifish/media/lua/client/FishingZonesApplyClassification.lua
+++ Contents/mods/peifish/media/lua/client/FishingZonesApplyClassification.lua
@@ -3,17 +3,14 @@ local FishingZonesApplyClassifications = {}
 require "client/Fishing/FishingManager"
 
 function FishingZonesApplyClassifications.apply()
-
     local fishes = Fishing.fishes
-
     Fishing.fishesByZone = {}
-
     for _,fish in pairs(fishes) do
-
         local classification = fish.classification
-        Fishing.fishesByZone[classification] = Fishing.fishesByZone[classification] or {}
-
-        table.insert(Fishing.fishesByZone[classification], fish)
+        if classification then
+            Fishing.fishesByZone[classification] = Fishing.fishesByZone[classification] or {}
+            table.insert(Fishing.fishesByZone[classification], fish)
+        end
     end
 end
 
